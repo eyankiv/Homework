2aeba936efb9643c115de156e13d62d3f271ab06
eyankiv <eyankiv@gmail.com>
2016-12-13 23:54:18 +0200

    SlidingPuzzle, the buttons displaying but the logic not setup yet .

---
+++ Android/AndroidHw/HW32.4/app/src/main/java/com/example/yevgeni/hw324/SlidingPuzzle.java
@@ -0,0 +1,38 @@
+package com.example.yevgeni.hw324;
+
+/**
+ * Created by yevgeni on 13/12/2016.
+ */
+
+public class SlidingPuzzle {
+    private int size;
+    private int emptyPiece;
+    private int movingPiece;
+
+    public SlidingPuzzle(int size) {
+        this.size = size;
+    }
+
+    public void setMovingPiece(int movingPiece) {
+        this.movingPiece = movingPiece;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+
+    public void setEmptyPiece(int emptyPiece) {
+        this.emptyPiece = emptyPiece;
+    }
+
+    public void makeAmove(int emptyPiece, int movingPiece){
+
+    }
+
+
+
+
+
+}
+
